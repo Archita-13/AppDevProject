71f1be1ed3a6e5b75d963d5d4f0e5eabb7ce9eb5
Archita-13 <88106920+Archita-13@users.noreply.github.com>
2021-07-29 03:21:47 +0530

    Select category activity

--- app/src/main/java/com/example/quizzler/Instructions.java
+++ app/src/main/java/com/example/quizzler/Instructions.java
@@ -2,13 +2,24 @@ package com.example.quizzler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Instructions extends AppCompatActivity {
-
+Button proceed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+        proceed = findViewById(R.id.button3);
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Instructions.this, SelectCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
