1dcd27a03f952898d1e38ef77a0ebd16e4292e43
Archita-13 <88106920+Archita-13@users.noreply.github.com>
2021-07-29 22:07:26 +0530

    bts pages completed

--- app/src/main/java/com/example/quizzler/BtsActivity.java
+++ app/src/main/java/com/example/quizzler/BtsActivity.java
@@ -17,11 +17,12 @@ Button confirm;
 RadioGroup radio_g;
 RadioButton op1,op2,op3,op4;
 
-String[] questions = {"What is the name of BTS's debut album?","Which was BTS's first show?","On which Award show did BTS make their US TV performance debut?","Which date is ARMY's birthday?","Name of the singer is?"};
 
-String[] answers = {"2 Cool 4 Skool","Rookie King: Channel Bangtan","American Music Awards","July 9","Jimin"};
+String[] questions = {"What is the name of BTS's debut album?","Which was BTS's first show?","On which Award show did BTS make their US TV performance debut?","Which date is ARMY's birthday?","Who is the youngest member of the band?"};
 
-String[] opt ={"2 Cool 4 Skool","We are Bulletproof","Skool Luv Affair","O!RUL8,2?","Run BTS","BTS Gayo","Rookie King: Channel Bangtan","American Hustle Life","American Music Awards","Billboard Music Awards","The Grammys","Teen Choice Awards","June 12","June 13","July 13","July 9","Jungkook","Jimin","Jin","J-Hope"};
+String[] answers = {"2 Cool 4 Skool","Rookie King: Channel Bangtan","American Music Awards","July 9","Jungkook"};
+
+String[] opt ={"2 Cool 4 Skool","We are Bulletproof","Skool Luv Affair","O!RUL8,2?","Run BTS","BTS Gayo","Rookie King: Channel Bangtan","American Hustle Life","American Music Awards","Billboard Music Awards","The Grammys","Teen Choice Awards","June 12","June 13","July 13","July 9","Jungkook","Suga","Jin","V"};
 
 int flag=0;
 public static int marks=0,correct=0,wrong=0;
@@ -35,13 +36,13 @@ public static int marks=0,correct=0,wrong=0;
         confirm = findViewById(R.id.confirm);
         radio_g = findViewById(R.id.radioGroup);
         op1 = findViewById(R.id.option1);
-        op1.setText(opt[flag]);
+        op1.setText(opt[flag+1]);
         op2 = findViewById(R.id.option2);
-        op2.setText(opt[flag]);
+        op2.setText(opt[flag+2]);
         op3 = findViewById(R.id.option3);
-        op3.setText(opt[flag]);
+        op3.setText(opt[flag+3]);
         op4 = findViewById(R.id.option4);
-        op4.setText(opt[flag]);
+        op4.setText(opt[flag+4]);
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,6 +73,7 @@ public static int marks=0,correct=0,wrong=0;
                 {
                     marks = correct;
                     Intent intent = new Intent(BtsActivity.this,BTSResultActivity.class);
+
                     startActivity(intent);
                 }
                 radio_g.clearCheck();
