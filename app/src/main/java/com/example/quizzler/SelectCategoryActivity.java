0a994cf66d4eaae0a288d7f5ebb61113b5dadf12
Archita-13 <88106920+Archita-13@users.noreply.github.com>
2021-07-29 12:31:46 +0530

    ignore me

--- app/src/main/java/com/example/quizzler/SelectCategoryActivity.java
+++ app/src/main/java/com/example/quizzler/SelectCategoryActivity.java
@@ -2,13 +2,24 @@ package com.example.quizzler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectCategoryActivity extends AppCompatActivity {
-
+Button bts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_category);
+        bts = findViewById(R.id.bts);
+        bts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectCategoryActivity.this, BtsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
